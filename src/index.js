c7c71ef8c263c15e7c2a3b249ce0f670adaf8a99
Adrian Guerrero <adrianjguerrero87@gmail.com>
2021-01-20 07:44:39 -0400

    creado rutas y sus componentes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import WebFont from 'webfontloader'
 import {BrowserRouter,Route,Switch} from 'react-router-dom'
+
+import './index.css';
 import Contenedor from './elements/Contenedor'
+
+import App from './App';
+import Login from './components/Login'
+import Register from './components/Register'
+import ExpensesByCategory from './components/ExpensesByCategory'
+import ExpensesList from './components/ExpensesList'
+import EditExpense from './components/EditExpense'
 
 WebFont.load({
   google: {
@@ -17,8 +24,15 @@ const Index = () => {
 
     <BrowserRouter>
       <Contenedor>
-
-        <App/> 
+        <Switch>
+          <Route path="/login" component={Login}/>
+          <Route path="/register" component={Register}/>
+          <Route path="/categories" component={ExpensesByCategory}/>
+          <Route path="/list" component={ExpensesList}/>
+          <Route path="/edit/:id" component={EditExpense}/>
+          <Route path="/" component={App}/>
+          
+        </Switch>
       </Contenedor>
     </BrowserRouter>
   );
