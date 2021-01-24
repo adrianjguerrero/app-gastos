f5fe43f39bacbea971bb781e9772f69b69124468
Adrian Guerrero <adrianjguerrero87@gmail.com>
2021-01-24 17:45:20 -0400

    header de app y gastos por categoria

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import {Helmet} from 'react-helmet'
+import  {Header, Titulo, ContenedorBotones, ContenedorHeader} from './elements/Header'
+import Boton from './elements/Boton'
 
 function App() {
   return (
-    <div className="">
+    <>
+      <Helmet>
+        <title>Agregar gasto</title>
+      </Helmet>
+      <Header>
+        <ContenedorHeader>
+          <Titulo>Agregar gasto</Titulo>
+          <ContenedorBotones>
+            <Boton to="/categories">Categorias</Boton>
+            <Boton to="/list">Lista de gastos</Boton>
+            <Boton>Logout</Boton>
+          </ContenedorBotones>
+        </ContenedorHeader>
+      </Header>
   
-    </div>
+    </>
   );
 }
 
--- src/components/ExpensesByCategory.js
+++ src/components/ExpensesByCategory.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import {Helmet} from 'react-helmet'
+import  {Header, Titulo, ContenedorBotones, ContenedorHeader} from './../elements/Header'
 
 const ExpensesByCategory = () => {
-    return ( <h1>ExpensesByCategory</h1> );
+    return ( 
+        <>
+        <Helmet>
+          <title>Gasto por categoria</title>
+        </Helmet>
+        <Header>
+
+          <ContenedorHeader>
+            <Titulo>Gastos por categoría</Titulo>
+          </ContenedorHeader>
+        </Header>
+    
+      </>
+     );
 }
  
 export default ExpensesByCategory
